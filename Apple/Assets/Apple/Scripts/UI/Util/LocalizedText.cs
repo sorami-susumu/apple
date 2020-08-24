ced7b1cca51c9acf42bd85196a19184402a128db
sorami-susumu <sorami.susumu@gmail.com>
2020-08-24 18:19:41 +0900

    make title

---
+++ Apple/Assets/Apple/Scripts/UI/Util/LocalizedText.cs
@@ -0,0 +1,24 @@
+using Apple.Domain.Model;
+using Floppy;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Apple.UI.Util
+{
+    public class LocalizedText : MonoBehaviour
+    {
+        [SerializeField, Multiline(5)]
+        private string Japanese;
+        [SerializeField, Multiline(5)]
+        private string English;
+        [SerializeField]
+        private Text text;
+
+        private void Awake()
+        {
+            Disk.Instance.Load<User>("user");
+            text.text = Disk.Instance.Get<User>("user").Language == "en" ? English : Japanese;
+        }
+    }
+}
+    
